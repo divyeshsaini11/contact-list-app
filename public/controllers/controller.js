a951a4fb36b1d25eb82ec0d16cec174630c00ab8
divyeshsaini11 <techdivyesh11@gmail.com>
2016-01-31 00:24:32 +0530

    Getting data from server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,23 +1,9 @@
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
 	console.log("Hello World from Controller");
 
-	person1 = {
-		name:'Siddharth',
-		email: 'siddharth@email.com',
-		number: '9867382921'
-	};
-	person2 = {
-		name:'Manoj',
-		email: 'manoj@email.com',
-		number: '8472208831'
-	};
-	person3 = {
-		name:'Divyesh',
-		email: 'divyesh@email.com',
-		number: '9837289196'
-	};
-
-	var contactList = [person1, person2, person3];
-
-	$scope.contactList = contactList;	
+$http.get('/contactList').success(function(response){
+	console.log("I got the data I requested");
+	$scope.contactList = response;
+}); 
+	
 }
--- server.js
+++ server.js
@@ -3,5 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList',function(req, res){
+	console.log("I received a GET request");
+
+	person1 = {
+		name:'Siddharth',
+		email: 'siddharth@email.com',
+		number: '9867382921'
+	};
+	person2 = {
+		name:'Manoj',
+		email: 'manoj@email.com',
+		number: '8472208831'
+	};
+	person3 = {
+		name:'Divyesh',
+		email: 'divyesh@email.com',
+		number: '9837289196'
+	};
+
+	var contactList = [person1, person2, person3];
+
+	res.json(contactList);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
