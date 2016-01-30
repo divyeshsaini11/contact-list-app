30ab82e078f24fe1655ad23fea3aa6f8ae4eb77e
divyeshsaini11 <techdivyesh11@gmail.com>
2016-01-30 23:59:13 +0530

    Added dummy data to test tables

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,23 @@
-function AppCtrl() {
+function AppCtrl($scope) {
 	console.log("Hello World from Controller");
+
+	person1 = {
+		name:'Siddharth',
+		email: 'siddharth@email.com',
+		number: '9867382921'
+	};
+	person2 = {
+		name:'Manoj',
+		email: 'manoj@email.com',
+		number: '8472208831'
+	};
+	person3 = {
+		name:'Divyesh',
+		email: 'divyesh@email.com',
+		number: '9837289196'
+	};
+
+	var contactList = [person1, person2, person3];
+
+	$scope.contactList = contactList;	
 }
